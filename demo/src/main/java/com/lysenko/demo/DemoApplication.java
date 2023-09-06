fa42d69d8baf4799f94a6a7dce41aec0135efad7
Anton Lutsenko <lutsenko.anton@gmail.com>
2023-09-06 13:32:56 +0300

    remark

--- demo/src/main/java/com/lysenko/demo/DemoApplication.java
+++ demo/src/main/java/com/lysenko/demo/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	* Your project doesn't have a structure (services, models, controllers etc)
+	**/
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
